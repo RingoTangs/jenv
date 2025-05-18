11537eaad763f799d659e331b06b25d0e4c929dc
RingoTangs <singrainyang@gmail.com>
2025-05-18 17:21:32 +0800

    build(eslint): 调整 ESLint 配置以适应项目需求
    - 在全局规则中添加 'no-console' 和 'unused-imports/no-unused-imports' 规则
    - 移除 'antfu/top-level-function' 规则
    - 为 TypeScript 文件添加 'style/brace-style' 规则
    - 为特定文件配置 'ts/explicit-function-return-type' 规则
    - 更新配置结构以支持多文件类型

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,25 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu({
-  type: 'lib',
-  typescript: true,
-  markdown: false,
-  rules: {
-    'no-console': ['off'],
-    'unused-imports/no-unused-imports': ['error'],
-    'antfu/top-level-function': ['off'],
-    'ts/explicit-function-return-type': ['warn'],
+export default antfu(
+  {
+    type: 'lib',
+    typescript: true,
+    markdown: false,
+    rules: {
+      'no-console': ['error'],
+      'unused-imports/no-unused-imports': ['error'],
+      'antfu/top-level-function': ['off'],
+      'style/brace-style': ['warn', '1tbs', { allowSingleLine: false }],
+    },
   },
-})
+  {
+    files: ['*.ts', '*.tsx', '*.vue'],
+    rules: {
+      'ts/explicit-function-return-type': ['warn'],
+    },
+  },
+  {
+    'files': ['src/utils/console.ts'],
+    'no-console': ['off'],
+  },
+)
